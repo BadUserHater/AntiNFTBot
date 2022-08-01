a51f3a5902a3d03eefd6f475246ac85d4fad4460
BadUserHater <105019950+BadUserHater@users.noreply.github.com>
2022-07-31 21:36:53 -0700

    BOT UPDATE #3 (Configuration)
    This version allows you to set up wheither if your server wants the bot to Auto-Ban members who sends NFT advertisements or not. For any issues, open u

--- README.txt
+++ README.txt
@@ -5,6 +5,8 @@ Anti-NFT bot is a open source discord.py bot that stops links to NFT websites, b
 even NFT based discord servers.
 Can also remove messages containing links to many NFT marketplaces.
 
+This version also lets you set in your server if you want the bot to auto-ban members who sends NFT advertisements or not.
+
 ================================================
 
 SETUP:
@@ -13,7 +15,14 @@ SETUP:
 - Fork or download this source code
 - client.run("BOTTOKENHERE") where BOTTOKENHERE is your Discord Bot Token that you got from the Discord developer portal
 - client = commands.Bot(command_prefix = "nft!") where nft! would be the prefix you want
--- When you invite the bot, the permissions needed is: Read Messages, Send Messages, Manage Messages, View Channel History, and Embed Links
+- SETTING UP STAFF
+>>>> Towards the bottom, head to botstaff = [757827467657478245]
+>>>> In the botstaff brackets, replace 757827467657478245 with the discord user ids of your staff you want to control the bot.
+>>>> To have more then one, seperate each user id with a comma and space. EXAMPLE: botstaff = [757827467657478245, 672273762892251136, 972161452133728256]
+- Invite your bot to the server
+>>>> When you invite the bot, the permissions needed is: Read Messages, Send Messages, Manage Messages, View Channel History, and Embed Links
+
+With the json files, you don't need to touch them.
 
 =================================================
 
@@ -26,6 +35,8 @@ How it works is we have 2 blacklist.txt files so the bot can trigger different f
 blacklist.txt is for the bot to block NFT market websites.
 blacklist2.txt is for the bot to block Pro NFT discord servers (like NFT project discord servers) and it responds differently.
 
+With rewrites and major updates, they come with bugs, if you encounter a bug in the code, then open an issue.
+
 =================================================
 
 THANKS:
@@ -35,9 +46,9 @@ And have the Anti-NFT bot be better.
 
 =================================================
 
-WARNING: THERE IS LITERALLY NO WARRENTY IF YOU DECIDE TO SELF-HOST THIS BOT.
-IF YOU DO, WE WILL NOT BE HELD RESPONSIBLE FOR DAMAGES. IF YOU DO SELF
-HOST THIS BOT, WE EXPECT YOU TO HAVE ENOUGH KNOWLEDGE IN DISCORD.PY AND PYTHON
+WARNING: THERE IS LITERALLY NO WARRENTY IF YOU DECIDE TO SELF-HOST THIS BOT. IF YOU DO, WE WILL NOT BE HELD RESPONSIBLE FOR
+DAMAGES. IF YOU DO SELF HOST THIS BOT, WE EXPECT YOU TO HAVE ENOUGH KNOWLEDGE IN DISCORD.PY AND PYTHON. WE WILL NOT HELP
+yOU SET THE BOT UP EITHER!
 
 LICENSE: We are using the MIT license so you can do whatever you want with this source code.
 Crediting me is also advised.
@@ -49,7 +60,11 @@ ORIGNAL BOT: https://discord.com/api/oauth2/authorize?client_id=9859985625738322
 SUPPORT SERVER: https://discord.gg/NpRbKaFmJU
 WEBSITE: https://meowbahhbot.dscdevstudios.xyz/antinftbot.html
 
-THE DEVELOPER'S YOUTUBE CHANNEL: https://www.youtube.com/c/TheIdiotCreatureHater
-FOLLOW THE DEV ON TWITTER: https://twitter.com/BadUserHater
+DEVELOPER'S LINKS (You can support me just by following me)
+YOUTUBE CHANNEL: https://www.youtube.com/c/TheIdiotCreatureHater
+TWITTER: https://twitter.com/BadUserHater
+GITHUB: https://github.com/BadUserHater
+GAMEJOLT: https://gamejolt.com/@IdiotCreatureHater
+DISCORD SERVER: https://discord.gg/nVs4rCNwyc
 SUBREDDIT: https://www.reddit.com/r/BadUserHaters/
-OUR TIKTOK: https://www.tiktok.com/@theidiotcreaturehater
+TIKTOK: https://www.tiktok.com/@theidiotcreaturehater
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import sys
 
 client = commands.Bot(command_prefix="nft!")
 client.remove_command("help")
@@ -16,28 +17,42 @@ async def on_message(message):
         return
     if client.user.id != message.author.id:
         if message.content in blacklist:
-            embed = discord.Embed(title="NO NFT ADVERTISEMENTS ALLOWED", description=f"HEY {member.mention} NO NFT ADVERTISEMENTS IS ALLOWED HERE. NFTS are causing harm to the world. Learn more at nft!nfttruth", color=(40959))
-            embed.add_field(name="CASE DETAILS", value=f'NFT Advertiser Mention: {member.mention}\nNFT Advertising Type: NFT Website.', inline=False)
-            await message.channel.send(embed=embed)
-            await message.delete()
+            if message.guild.id in client.punishments:
+                embed = discord.Embed(title="NO NFT ADVERTISEMENTS ALLOWED", description=f"HEY {member.mention} NO NFT ADVERTISEMENTS IS ALLOWED HERE. NFTS are causing harm to the world. Learn more at nft!nfttruth", color=(40959))
+                embed.add_field(name="CASE DETAILS", value=f'NFT Advertiser Mention: {member.mention}\nNFT Advertising Type: NFT Website.', inline=False)
+                embed.set_footer(text="Punishment has been enables so the user has been banned")
+                await message.channel.send(embed=embed)
+                await message.delete()
+                await message.author.ban(reason="NFT Advertisements [Auto by Anti-NFT Bot]")
+            else:
+                embed = discord.Embed(title="NO NFT ADVERTISEMENTS ALLOWED", description=f"HEY {member.mention} NO NFT ADVERTISEMENTS IS ALLOWED HERE. NFTS are causing harm to the world. Learn more at nft!nfttruth", color=(40959))
+                embed.add_field(name="CASE DETAILS", value=f'NFT Advertiser Mention: {member.mention}\nNFT Advertising Type: NFT Website.', inline=False)
+                await message.channel.send(embed=embed)
+                await message.delete()
         if message.content in blacklist2:
-            embed = discord.Embed(title="NO NFT ADVERTISEMENTS ALLOWED", description=f"Hey {member.mention}, it seems like you are attempting to advertise a NFT based discord server. Look at nft!nfttruth to find out the truth on NFTs", color=(40959))
-            embed.add_field(name="CASE DETAILS", value=f'NFT Advertiser Mention: {member.mention}\nNFT Advertising Type: Discord Server', inline=False)
-            embed.set_footer(text="If you are the owner of the server and believe this is a mistake, contact the owner in our support server to appeal. (Avaliable in nft!info)")
-            await message.channel.send(embed=embed)
-            await message.delete()
+            if message.guild.id in client.punishments:
+                embed = discord.Embed(title="NO NFT ADVERTISEMENTS ALLOWED", description=f"Hey {member.mention}, it seems like you are attempting to advertise a NFT based discord server. Well this server is protected by Anti-NFT Bot. Look at nft!nfttruth to find out the truth on NFTs", color=(40959))
+                embed.add_field(name="CASE DETAILS", value=f'NFT Advertiser Mention: {member.mention}\nNFT Advertising Type: Discord Server', inline=False)
+                embed.set_footer(text="If you are the owner of the server and believe this is a mistake and is Anti-NFT or not NFT based, contact the owner in our support server to appeal. (Avaliable in nft!info)")
+                embed.set_footer(text="Punishment has been enables so the user has been banned")
+                await message.channel.send(embed=embed)
+                await message.delete()
+                await message.author.ban(reason="NFT Advertisements [Auto by Anti-NFT Bot]")
+            else:
+                embed = discord.Embed(title="NO NFT ADVERTISEMENTS ALLOWED", description=f"Hey {member.mention}, it seems like you are attempting to advertise a NFT based discord server. Well this server is protected by Anti-NFT Bot. Look at nft!nfttruth to find out the truth on NFTs", color=(40959))
+                embed.add_field(name="CASE DETAILS", value=f'NFT Advertiser Mention: {member.mention}\nNFT Advertising Type: Discord Server', inline=False)
+                embed.set_footer(text="If you are the owner of the server and believe this is a mistake and is Anti-NFT or not NFT based, contact the owner in our support server to appeal. (Avaliable in nft!info)")
+                await message.channel.send(embed=embed)
+                await message.delete()
         
     await client.process_commands(message)
-
-
-
-
 
 @client.command()
 async def help(ctx):
     embed = discord.Embed(title="ANTI-NFT Bot", description="Here is my entire Commands List", color=(40959))
     embed.add_field(name="GENERAL COMMANDS", value='nft!help - This message\nnft!ping - Checks my Latency\nnft!info - Information about the Bot\nnft!privacy - Checks my privacy Policy', inline=False)
-    embed.add_field(name="ANTI-NFT", value='nft!nfttruth - Tells you the truth about NFTs', inline=False)
+    embed.add_field(name="ANTI-NFT", value='nft!nfttruth - Tells you the truth about NFTs\nnft!whatisanft - I share what a NFT is from Urban Dictionary', inline=False)
+    embed.add_field(name="DEVELOPER-ONLY COMMANDS", value='nft!restart - Reboots the bot and loads in the new commands/content.', inline=False)
     await ctx.send(embed=embed)
 
 @client.command()
@@ -46,16 +61,18 @@ async def ping(ctx):
 
 @client.command()
 async def info(ctx):
-    embed = discord.Embed(title="ANTI-NFT Bot", description="Information about the NFT discord bot.", color=(40959))
+    embed = discord.Embed(title="ANTI-NFT Bot", description="Information about the Anti-NFT discord bot.", color=(40959))
     embed.add_field(name="GENERAL", value='CREATOR: Idiot Creature Hater#2255', inline=False)
-    embed.add_field(name="LINKS", value='Invite me: https://discord.com/api/oauth2/authorize?client_id=985998562573832262&permissions=274879310912&scope=bot\nSupport Server: https://discord.gg/NpRbKaFmJU\nSource Code: https://github.com/BadUserHater/AntiNFTBot', inline=False)
-    embed.add_field(name="SOURCE CODE STATUS/CREDITS", value='Open Source?: TRUE\nOrignal Creator: Idiot Creature Hater#2255\nLicense: MIT\nSource Code: https://github.com/BadUserHater/AntiNFTBot\nORIGNAL BOT: https://discord.com/oauth2/authorize?client_id=985998562573832262&permissions=274879310912&scope=bot\nWEBSITE: https://meowbahhbot.dscdevstudios.xyz/antinftbot.html', inline=False)
+    embed.add_field(name="LINKS", value='Invite me: https://discord.com/api/oauth2/authorize?client_id=985998562573832262&permissions=274879310912&scope=bot\nSupport Server: https://discord.gg/xTfAYs7Kyg\nSource Code: https://github.com/BadUserHater/AntiNFTBot', inline=False)
+    embed.add_field(name="CREDITS/SOURCE", value='Orignal Creator: Idiot Creature Hater#2255\nOpen Source?: TRUE\nLicense: MIT\nSource Code: https://github.com/BadUserHater/AntiNFTBot', inline=False)
     await ctx.send(embed=embed)
 
 @client.command()
 async def privacy(ctx):
-    embed = discord.Embed(title="ANTI-NFT Bot Privacy Policy", description="Last updated: July 4, 2022", color=(40959))
-    embed.add_field(name="WHAT DATA DO YOU COLLECT", value='Anti-NFT Bot does not log, collect, or store any data. Once you invite the bot to your server, it is ready to search for any NFT advertisement links or any phrases relating to NFT advertisements.', inline=False)
+    embed = discord.Embed(title="ANTI-NFT Bot Privacy Policy", description="Last updated: July 23, 2022", color=(40959))
+    embed.add_field(name="WHAT DATA DO YOU COLLECT", value='Anti-NFT Bot does not store any data by default, however if you use **nft!punishenable**, your server ID will be collected.', inline=False)
+    embed.add_field(name="WHY IS THIS DATA NEEDED?", value='The data is needed so the bot knows if a member should be punished if they send a NFT Advertisement in your server.', inline=False)
+    embed.add_field(name="HOW DO I REMOVE MY DATA?", value='To remove the server ID it collected, then execute **nft!punishdisable**. If you kick the bot before doing this, then please reach out to the owner of the bot to remove your data.', inline=False)
     embed.add_field(name="HOW MAY I CONTACT YOU FOR CONCERNS", value='Reach to use at our support server at: https://discord.gg/NpRbKaFmJU', inline=False)
     await ctx.send(embed=embed)
 
@@ -78,7 +95,70 @@ if "__main__" == __name__:
     with open("blacklist2.txt", "r") as f:
         blacklist2 = f.read().splitlines()
 
+@client.command()
+@commands.has_permissions(administrator=True)
+@commands.bot_has_permissions(ban_members=True)
+async def punishenable(ctx):
+    client.punishments.append(ctx.guild.id)
+    data = read_json("punishment")
+    data["ActivePunishmentS"].append(ctx.guild.id)
+    write_json(data, "punishment")
+    await ctx.send("The Punishment Module has been enabled. I will **now start** banning members if they send a NFT advertisement in this server")
 
+@punishenable.error
+async def punishenable_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send('Sorry but you need the **Administrator** permission to use this command')
+    if isinstance(error, commands.BotMissingPermissions):
+        await ctx.send("Sorry but I need the **Ban Members** permission for this function to work.")
+    else:
+        raise error
+
+@client.command()
+@commands.has_permissions(administrator=True)
+async def punishdisable(ctx):
+    client.punishments.remove(ctx.guild.id)
+    data = read_json("punishment")
+    data["ActivePunishmentS"].remove(ctx.guild.id)
+    write_json(data, "punishment")
+    await ctx.send("The Punishment Module has been **disabled**. I will **no longer be** banning members if they send a NFT advertisement in this server")
+
+@punishdisable.error
+async def punishdisable_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send('Sorry but you need the **Administrator** permission to use this command')
+    else:
+        raise error
+
+def read_json(filename):
+    with open(f"./punishment.json", "r") as file:
+        data = json.load(file)
+    return data
+
+def write_json(data, filename):
+    with open(f"./punishment.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+
+
+
+
+
+
+botstaff = [757827467657478245]
+
+def restart_program():
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+    
+@client.command()
+async def restart(ctx):
+    if ctx.author.id in botstaff:
+        await ctx.send("Restarting... This may take some time.")
+        restart_program()
+    else:
+        await ctx.send("This command can only be used by the bot developers")
+        return
 
 
 
